6fb5bf63d0642cc16afffe259fac37a7162e93b2
marwan mohamed <138940689+MarwanMohamed12@users.noreply.github.com>
2024-12-02 22:31:50 +0200

    Update PCIe_sequencer_AUL2.sv

--- Testbench/PCIe_sequencer_AUL2.sv
+++ Testbench/PCIe_sequencer_AUL2.sv
@@ -4,12 +4,6 @@
 // 
 ////////////////////////////////////////////////////////////////////////////////
 
-
-package PCIe_sequencer_AUL2_pkg;
-    import uvm_pkg::*;
-        `include "uvm_macros.svh"
-    import PCIe_Seq_Item_pkg::*;
-
     class PCIe_sequencer_AUL2 extends uvm_sequencer #(PCIe_Seq_Item);
         `uvm_component_utils(PCIe_sequencer_AUL2)
 
@@ -20,4 +14,3 @@ package PCIe_sequencer_AUL2_pkg;
     function PCIe_sequencer_AUL2::new(string name="PCIe_sequencer_AUL2",uvm_component phase);
             super.new(name,phase);
     endfunction
-endpackage
