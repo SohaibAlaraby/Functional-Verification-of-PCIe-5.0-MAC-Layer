e8d7cf08d9ba1630f0fa07d28c5a8e8d4badcffe
marwan mohamed <138940689+MarwanMohamed12@users.noreply.github.com>
2025-02-03 18:22:22 +0200

    Update PCIe_pkg.sv

--- TestBench/PCIe_pkg.sv
+++ TestBench/PCIe_pkg.sv
@@ -8,15 +8,25 @@ package PCIe_pkg;
     `include "LTSSM1_seq_item.sv"
     `include "LTSSM2_seq_item.sv"
     `include "PIPE_seq_item.sv"
+    `include "TX_Master_seq_item.sv"
+  
     `include "Reset_LTSSM1_seq.sv"
     `include "Reset_LTSSM2_seq.sv"
     `include "LinkUp1_seq.sv"
     `include "LinkUp2_seq.sv"
-
+    `include "TLP_Seq_TX_MASTER_D.sv"
+    `include "TLP_Seq_TX_MASTER_U.sv"
+    `include "DLLP_Seq_TX_MASTER_D.sv"
+    `include "DLLP_Seq_TX_MASTER_U.sv"
+    `include "DLLP_TLP_Seq_TX_MASTER_D.sv"
+    `include "DLLP_TLP_Seq_TX_MASTER_U.sv"
+  
     `include "RX_Slave_Config.sv"
     `include "TX_Slave_Config.sv"
     `include "LTSSM1_Config.sv"
     `include "LTSSM2_Config.sv"
+    `include "TX_Master_Config.sv"
+    `include "RX_Passive_Config.sv"
 
     `include "RX_Slave_D_Monitor.sv"
     `include "RX_Slave_D_Driver.sv"
@@ -31,8 +41,20 @@ package PCIe_pkg;
     `include "TX_Slave_U_Monitor.sv"
     `include "TX_Slave_U_Agent.sv"
     
+    `include "TX_Master_D_Sequencer.sv"
+    `include "TX_Master_D_Driver.sv"
+    `include "TX_Master_D_Agent.sv"
 
+    `include "TX_Master_U_Sequencer.sv"
+    `include "TX_Master_U_Driver.sv"
+    `include "TX_Master_U_Agent.sv"
+  
+    `include "RX_Passive_D_Monitor.sv"
+    `include "RX_Passive_D_Agent.sv"
 
+    `include "RX_Passive_U_Monitor.sv"
+    `include "RX_Passive_U_Agent.sv"
+  
     `include "LTSSM1_D_Driver.sv"
    // `include "LTSSM1_D_Monitor.sv"
     `include "LTSSM1_D_Sequencer.sv"
@@ -59,7 +81,11 @@ package PCIe_pkg;
 
     `include "LinkUp1_vseq.sv"
     `include "LinkUp2_vseq.sv"
-    
+  
+    `include "TLP_vSeq_TX_MASTER.sv"
+    `include "DLLP_vSeq_TX_MASTER.sv"
+    `include "DLLP_TLP_vSeq_TX_MASTER.sv"
+  
     `include "Adapter.sv"
 
     `include "PCIe_Env.sv"
