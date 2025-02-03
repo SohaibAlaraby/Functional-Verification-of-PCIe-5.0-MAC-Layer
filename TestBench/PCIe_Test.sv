4c667e1c68bca13027697019e2b001f033d00453
marwan mohamed <138940689+MarwanMohamed12@users.noreply.github.com>
2025-02-03 18:37:21 +0200

    Update PCIe_Test.sv

--- TestBench/PCIe_Test.sv
+++ TestBench/PCIe_Test.sv
@@ -11,7 +11,11 @@ class PCIe_Test  extends uvm_test;
   
   LinkUp1_vseq      LinkUp1_vseq_h;
   LinkUp2_vseq      LinkUp2_vseq_h;
-  
+
+  DLLP_TLP_vSeq_TX_MASTER DLLP_TLP_vSeq_TX_MASTER_h;
+  TLP_vSeq_TX_MASTER TLP_vSeq_TX_MASTER_h;
+  DLLP_vSeq_TX_MASTER DLLP_vSeq_TX_MASTER_h;
+	
  
   TX_Slave_Config   TX_Slave_U_Config_h;
   RX_Slave_Config   RX_Slave_U_Config_h;
@@ -27,7 +31,19 @@ class PCIe_Test  extends uvm_test;
 
   LTSSM1_Config     LTSSM1_D_Config_h;
   LTSSM2_Config     LTSSM2_D_Config_h; 
-   
+
+  TX_Slave_Config   TX_Slave_D_Config_h;
+  RX_Slave_Config   RX_Slave_D_Config_h;
+ 
+
+  LTSSM1_Config     LTSSM1_D_Config_h;
+  LTSSM2_Config     LTSSM2_D_Config_h; 
+  
+  TX_Master_Config  TX_Master_D_Config_h;
+  TX_Master_Config  TX_Master_U_Config_h;
+
+  RX_Passive_Config RX_Passive_D_Config_h;
+  RX_Passive_Config RX_Passive_U_Config_h;
 
 
 
@@ -62,7 +78,11 @@ class PCIe_Test  extends uvm_test;
 
          LinkUp1_vseq_h = LinkUp1_vseq::type_id::create("LinkUp1_vseq_h");
 	 LinkUp2_vseq_h = LinkUp2_vseq::type_id::create("LinkUp2_vseq_h");
-	 
+
+	 DLLP_vSeq_TX_MASTER_h = DLLP_vSeq_TX_MASTER::type_id::create("DLLP_vSeq_TX_MASTER_h");
+	 TLP_vSeq_TX_MASTER_h = TLP_vSeq_TX_MASTER::type_id::create("TLP_vSeq_TX_MASTER_h");
+	 DLLP_TLP_vSeq_TX_MASTER_h = DLLP_TLP_vSeq_TX_MASTER::type_id::create("DLLP_TLP_vSeq_TX_MASTER_h");  
+	  
 	 LTSSM1_U_Config_h = LTSSM1_Config::type_id::create("LTSSM1_U_Config_h");
 	 LTSSM2_U_Config_h = LTSSM2_Config::type_id::create("LTSSM2_U_Config_h");
 
@@ -75,7 +95,11 @@ class PCIe_Test  extends uvm_test;
 	 TX_Slave_U_Config_h = TX_Slave_Config::type_id::create("TX_Slave_U_Config_h");
 	 RX_Slave_U_Config_h = RX_Slave_Config::type_id::create("RX_Slave_U_Config_h");
 	  	  
+	 TX_Master_D_Config_h=TX_Master_Config::type_id::create("TX_Master_D_Config_h");
+	 TX_Master_U_Config_h=TX_Master_Config::type_id::create("TX_Master_U_Config_h");
 
+	 RX_Passive_D_Config_h = RX_Passive_Config::type_id::create("RX_Passive_D_Config_h"); 
+	 RX_Passive_U_Config_h = RX_Passive_Config::type_id::create("RX_Passive_U_Config_h");
 	 
 	 if(!uvm_config_db #(virtual PIPE_if)::get(this,"","PIPE_if_U_h",LTSSM1_U_Config_h.PIPE_vif_h))
 	          `uvm_fatal(get_type_name(),"can't get the PIPE interface")
@@ -89,8 +113,23 @@ class PCIe_Test  extends uvm_test;
 	 if(!uvm_config_db #(virtual LPIF_if)::get(this,"","LPIF_if_D_h",LTSSM2_D_Config_h.LPIF_vif_h))
 	          `uvm_fatal(get_type_name(),"can't get the LPIF interface")
 
+		 
+	 if(!uvm_config_db #(virtual LPIF_if)::get(this,"","LPIF_if_D_h",TX_Master_D_Config_h.LPIF_vif_h))
+	          `uvm_fatal(get_type_name(),"can't get the LPIF interface")
+	 
+	 if(!uvm_config_db #(virtual LPIF_if)::get(this,"","LPIF_if_U_h",TX_Master_U_Config_h.LPIF_vif_h))
+	          `uvm_fatal(get_type_name(),"can't get the LPIF interface")
 
-    
+
+		 
+    	if(!uvm_config_db #(virtual LPIF_if)::get(this,"","LPIF_if_U_h",RX_Passive_U_Config_h.LPIF_vif_h))
+	          `uvm_fatal(get_type_name(),"can't get the LPIF interface")
+	
+	
+	if(!uvm_config_db #(virtual LPIF_if)::get(this,"","LPIF_if_D_h",RX_Passive_D_Config_h.LPIF_vif_h))
+	          `uvm_fatal(get_type_name(),"can't get the LPIF interface")
+
+		
 	 if(!uvm_config_db #(virtual PIPE_if)::get(this,"","PIPE_if_D_h",TX_Slave_D_Config_h.PIPE_vif_h))
 	          `uvm_fatal(get_type_name(),"can't get the PIPE interface")
   
@@ -117,7 +156,15 @@ class PCIe_Test  extends uvm_test;
 
    uvm_config_db #(TX_Slave_Config)::set(this,"*","TX_Slave_U_Config_h",TX_Slave_U_Config_h);
    uvm_config_db #(RX_Slave_Config)::set(this,"*","RX_Slave_U_Config_h",RX_Slave_U_Config_h);
-   
+
+	   
+   uvm_config_db #(TX_Master_Config)::set(this,"*","TX_Master_D_Config_h",TX_Master_D_Config_h);
+   uvm_config_db #(TX_Master_Config)::set(this,"*","TX_Master_U_Config_h",TX_Master_U_Config_h);
+  
+
+   uvm_config_db #(RX_Passive_Config)::set(this,"*","RX_Passive_D_Config_h",RX_Passive_D_Config_h);
+   uvm_config_db #(RX_Passive_Config)::set(this,"*","RX_Passive_U_Config_h",RX_Passive_U_Config_h);
+
    
   endfunction :build_phase 
   
@@ -166,7 +213,15 @@ class PCIe_Test  extends uvm_test;
 	  	  LinkUp2_vseq_h.sqr_u1 = PCIe_Env_h.LTSSM2_D_Agent_h.LTSSM2_D_Sequencer_h;
 	  	  LinkUp2_vseq_h.sqr_u2 = PCIe_Env_h.LTSSM2_U_Agent_h.LTSSM2_U_Sequencer_h;	  	  
 	  	  
-	  	  
+	  	  DLLP_vSeq_TX_MASTER_h.sqr_d = PCIe_Env_h.TX_Master_D_Agent_h.TX_Master_D_Sequencer_h ;
+		  DLLP_vSeq_TX_MASTER_h.sqr_u = PCIe_Env_h.TX_Master_U_Agent_h.TX_Master_U_Sequencer_h ;
+
+		  TLP_vSeq_TX_MASTER_h.sqr_d = PCIe_Env_h.TX_Master_D_Agent_h.TX_Master_D_Sequencer_h ;
+		  TLP_vSeq_TX_MASTER_h.sqr_u = PCIe_Env_h.TX_Master_U_Agent_h.TX_Master_U_Sequencer_h ;
+
+		  DLLP_TLP_vSeq_TX_MASTER_h.sqr_d = PCIe_Env_h.TX_Master_D_Agent_h.TX_Master_D_Sequencer_h ;
+		  DLLP_TLP_vSeq_TX_MASTER_h.sqr_u = PCIe_Env_h.TX_Master_U_Agent_h.TX_Master_U_Sequencer_h ;
+
 	  	 fork
 	  	 
 	  	      LinkUp1_vseq_h.start(null);
@@ -174,7 +229,10 @@ class PCIe_Test  extends uvm_test;
 	     
 	         join
 	    
-	    
+	    	//TLP_vSeq_TX_MASTER_h.start(null);	
+	    	//DLLP_vSeq_TX_MASTER_h.start(null);
+		//DLLP_TLP_vSeq_TX_MASTER_h.start(null);
+	  
 	 phase.drop_objection(this);
   
   
